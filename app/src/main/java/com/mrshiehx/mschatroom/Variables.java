f7f05f6becb956c79af03a088aee2ce552f6fa6a
MrShieh-X <3553413882@qq.com>
2020-12-20 20:40:04 +0800

    20201220 9th commit alpha-3,for Variables.java

--- app/src/main/java/com/mrshiehx/mschatroom/Variables.java
+++ app/src/main/java/com/mrshiehx/mschatroom/Variables.java
@@ -2,19 +2,19 @@ package com.mrshiehx.mschatroom;
 
 public class Variables {
     //服务器地址
-    public static String SERVER_ADDRESS = "192.168.101.110";
+    public static String SERVER_ADDRESS = "xx.xx.xx.xx";
     //获得验证码时间（秒）
-    public static final int GET_CAPTCHA_TIME_SECOND = 5;
+    public static final int GET_CAPTCHA_TIME_SECOND = 60;
     //获得验证码时间（毫秒）
     public static final int GET_CAPTCHA_TIME = GET_CAPTCHA_TIME_SECOND * 1000;
     //MySQL数据库名称
-    public static String DATABASE_NAME = "mscr";
+    public static String DATABASE_NAME = "";
     //MySQL数据库账号
-    public static String DATABASE_USER = "root";
+    public static String DATABASE_USER = "";
     //MySQL数据库密码
-    public static String DATABASE_PASSWORD = "wwwwwww";
+    public static String DATABASE_PASSWORD = "";
     //MySQL表格名称
-    public static String DATABASE_TABLE_NAME = "users";
+    public static String DATABASE_TABLE_NAME = "";
     //作者电子邮箱地址（用于出现错误反馈给作者和关于页面联系作者）
     public static final String AUTHOR_MAIL = "bntoylort@outlook.com";
 
@@ -24,13 +24,13 @@ public class Variables {
     //拆分符号（不要使用字母和数字）
     public static final String SPLIT_SYMBOL = "&";
     //文本加密密钥（不能太长，也不能太短）
-    public static final String TEXT_ENCRYPTION_KEY = "fuckfuckfuckfuckfuckfuck";
+    public static final String TEXT_ENCRYPTION_KEY = "";
     //发送验证码邮件的邮箱的SMTP服务器地址
-    public static final String CAPTCHA_EMAIL_SMTP_SERVER_ADDRESS = "smtp.163.com";
+    public static final String CAPTCHA_EMAIL_SMTP_SERVER_ADDRESS = "smtp.xxx.com";
     //发送验证码邮件的邮箱地址
-    public static String CAPTCHA_EMAIL_ADDRESS = "mrsxservice@163.com";
+    public static String CAPTCHA_EMAIL_ADDRESS = "xx@xxx.com";
     //发送验证码邮件的邮箱的授权码
-    public static String AUTHENTICATOR = "TFXQXEGWNOJVWOVE";
+    public static String AUTHENTICATOR = "XXXXX";
     //作者的网站
     public static final String AUTHOR_WEBSITE_URL="https://mrshieh-x.github.io";
     //作者的GitHub地址
